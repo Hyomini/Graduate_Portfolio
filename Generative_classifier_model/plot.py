2ad76cc362f97a646585b6d232ab47dfafd2e07c
안효민 <iris0202@hanyang.ac.kr>
2019-07-30 11:48:39 +0900

    0730 final

--- Generative_classifier_model/plot.py
+++ Generative_classifier_model/plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def make_plot(ite,mahala_axis,euc_axis) :
-  plt.title("Time Comparison(mnist test set) - on CPU")
+  plt.title("Time Comparison(mnist test set) - on GPU")
   plt.xlabel('Iteration')
   plt.ylabel('Time')
   plt.plot(ite, mahala_axis, c="r", label="mahalanobis")
